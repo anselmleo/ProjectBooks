2f58ad9c6fabac97417f7a197e1f955cab05f6ba
Anselm <anselmleo@gmail.com>
2019-10-12 18:04:29 +0100

    create initial relevant routes

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@
 
 $router->get('/', function () use ($router) {
     return response()->json([
-        "message" => "Welcome to Timbala marketplace APIs",
+        "message" => "Welcome to Fotomi API endpoints",
         "base_url" => url('/') . "api/v1/"
     ]);
 });
 
 $router->group(['prefix' => 'api/v1'], function () use ($router) {
-    $router->post('worker-registration', 'AuthController@registerWorker');
+    $router->post('user-registration', 'AuthController@registerWorker');
     $router->post('employer-registration', 'AuthController@registerEmployer');
     $router->post('agent-registration', 'AuthController@registerAgent');
     $router->post('confirm-email', 'AuthController@confirmEmail');
@@ -29,51 +29,10 @@ $router->group(['prefix' => 'api/v1'], function () use ($router) {
     $router->patch('profile', 'UserController@profile');
     $router->post('work-history', 'UserController@workHistory');
 
-    $router->post('verify-bvn', 'UserController@bvnVerification');
-    $router->get('bvn-analysis', 'UserController@getBvnAnalysis');
-
-    $router->post('subscribe', 'UserController@subscribe');
-    $router->get('callback', 'UserController@paymentCallback');
-
-
-    $router->group(['prefix' => 'job-board'], function () use ($router) {
-        $router->post('', 'JobBoardController@createJob');
-
-        $router->get('', 'JobBoardController@myJobs');
-
-        $router->post('{id}/bid', 'JobBoardController@bid');
-
-        $router->patch('{id}/update', 'JobBoardController@updateJob');
-        $router->patch('{id}/hire-worker/{worker_id}', 'JobBoardController@hireWorker');
-        $router->patch('{id}/review-worker/{worker_id}', 'JobBoardController@reviewWorker');
-        $router->patch('{id}/review-employer/{employer_id}', 'JobBoardController@reviewEmployer');
-        $router->patch('{id}/complete', 'JobBoardController@completeJob');
-
-        $router->get('{id}/pitches', 'JobBoardController@getJobPitches');
-        $router->get('{id}/reviews', 'JobBoardController@jobReviews');
-    });
-
-    $router->get('job-listing', 'JobBoardController@jobListing');
-    $router->get('bid-status/{job_id}', 'JobBoardController@bidStatus');
-    $router->get('my-area-jobs', 'JobBoardController@myAreaJobs');
-    $router->get('top-jobs', 'JobBoardController@topJobs');
-    $router->get('dashboard-stat', 'JobBoardController@dashboardStat');
-
-    $router->group(['prefix' => 'utils'], function () use ($router) {
-        $router->get('cities', 'HomeController@getCities');
-        $router->get('categories', 'HomeController@getCategories');
-    });
+    
 
     $router->group(['prefix' => 'admin'], function () use ($router) {
         $router->get('all-users', 'UserController@allUsers');
         $router->patch('subscribe/{user_id}', 'UserController@manuallySubscribeUser');
-        $router->get('jobs', 'JobBoardController@allJobs');
-        $router->patch('jobs/{id}/approve', 'JobBoardController@approveJob');
-        $router->patch('jobs/{id}/reverse-approval', 'JobBoardController@unApproveJob');
-    });
-
-    $router->group(['prefix' => 'agent'], function () use ($router) {
-        $router->post('register-worker', 'UserController@registerWorkerByAgent');
-        $router->get('workers', 'UserController@getAgentWorkers');
     });
 });
